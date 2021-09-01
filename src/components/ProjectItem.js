76694e9a26142f14747176c67114ca72631a801b
Peter Bacenet <peterbacenet@icloud.com>
2021-08-31 22:47:08 -0400

    Completed Project w/ Lists and Keys

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const newObj = technologies.map((technology) => {
+    return (
+      <span key={technology}> {technology} </span>
+    )
+  })   
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      {newObj}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,17 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+
+  const singleProj = projects.map((proj)=> {
+    console.log(proj)
+    return ( <ProjectItem key={proj.id} name={proj.name} about={proj.about} technologies={proj.technologies} /> )})
+  console.log(singleProj);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {singleProj}
+      </div>
     </div>
   );
 }
